46823030756b591b52446ac13a1f594caded2da6
Jayllan Abecia <abeciaj23@gmail.com>
2025-03-02 18:37:24 +1100

    update script

--- docker/jenkins-agent/config.groovy
+++ docker/jenkins-agent/config.groovy
@@ -1,17 +1,52 @@
 import jenkins.model.*
 import com.cloudbees.hudson.plugins.folder.*
+import org.jenkinsci.plugins.workflow.job.WorkflowJob
+import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
+import hudson.plugins.git.*
+import jenkins.plugins.git.*
 
+// Define folder and job names
 def folderName = 'test-folder1'
 def jobName = 'test-ijob1'
+def gitUrl = 'https://github.com/abeciaj/hello-world.git'
+def branch = 'main' // Change this if your branch is different
 
-// Get the folder, create if not exists
+// Get Jenkins instance
 def jenkins = Jenkins.instance
-def folder = jenkins.getItem(folderName) ?: jenkins.createProject(Folder, folderName)
 
-// Create the job inside the folder if it doesn't exist
-if (folder.getItem(jobName) == null) {
-    def job = folder.createProject(hudson.model.FreeStyleProject, jobName)
-    println "Job '${jobName}' created inside folder '${folderName}'."
+// Get or create the folder
+def folder = jenkins.getItem(folderName)
+if (folder == null) {
+    folder = jenkins.createProject(Folder, folderName)
+    println "Folder '${folderName}' created."
 } else {
-    println "Job '${jobName}' already exists inside '${folderName}'."
+    println "Folder '${folderName}' already exists."
 }
+
+// Get or create the pipeline job inside the folder
+def job = folder.getItem(jobName)
+if (job == null) {
+    job = folder.createProject(WorkflowJob, jobName)
+    println "Pipeline job '${jobName}' created inside folder '${folderName}'."
+} else {
+    println "Pipeline job '${jobName}' already exists inside folder '${folderName}'. Updating settings..."
+}
+
+// Define GitHub repository as the SCM source
+def scm = new GitSCM(
+    [new UserRemoteConfig(gitUrl, null, null, null)], // Repo URL
+    [new BranchSpec(branch)], // Branch
+    false,
+    [],
+    null,
+    null,
+    [] // Additional behaviours
+)
+
+// Set the pipeline to use Jenkinsfile from GitHub
+def scmFlowDefinition = new CpsScmFlowDefinition(scm, "Jenkinsfile") // Looks for 'Jenkinsfile' in the repo
+job.definition = scmFlowDefinition
+
+// Save the job
+job.save()
+println "Pipeline job '${jobName}' in folder '${folderName}' is now connected to GitHub repo '${gitUrl}'."
