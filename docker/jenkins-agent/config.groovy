50191cec5afe2f02d647b2fe68811f302b35d3ac
Jayllan Abecia <abeciaj23@gmail.com>
2025-03-03 12:57:03 +1100

    update script

--- docker/jenkins-agent/config.groovy
+++ docker/jenkins-agent/config.groovy
@@ -1,23 +1,22 @@
 import jenkins.model.*
 import com.cloudbees.hudson.plugins.folder.*
 import org.jenkinsci.plugins.workflow.job.WorkflowJob
-import org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition
-import hudson.plugins.git.*
-import jenkins.plugins.git.*
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition
 
-// Define folder and job names
 def folderName = 'test-folder1'
 def jobName = 'test-ijob1'
-def gitUrl = 'https://github.com/abeciaj/hello-world.git'
-def branch = 'main' // Change this if your branch is different
+def BITBUCKET_REPO = 'https://abeciaj23@bitbucket.org/devopsph/hello-world.git'
+def GITHUB_REPO = 'https://github.com/abeciaj/hello-world.git'
 
 // Get Jenkins instance
-def jenkins = Jenkins.instance
+def jenkins = Jenkins.get()
 
 // Get or create the folder
 def folder = jenkins.getItem(folderName)
 if (folder == null) {
-    folder = jenkins.createProject(Folder, folderName)
+    folder = new Folder(jenkins, folderName)
+    jenkins.add(folder, folderName)  // Corrected line
+    jenkins.save()
     println "Folder '${folderName}' created."
 } else {
     println "Folder '${folderName}' already exists."
@@ -26,27 +25,90 @@ if (folder == null) {
 // Get or create the pipeline job inside the folder
 def job = folder.getItem(jobName)
 if (job == null) {
-    job = folder.createProject(WorkflowJob, jobName)
+    job = new WorkflowJob(folder, jobName)
+    folder.add(job, jobName)  // Corrected line
+    jenkins.save()
     println "Pipeline job '${jobName}' created inside folder '${folderName}'."
 } else {
-    println "Pipeline job '${jobName}' already exists inside folder '${folderName}'. Updating settings..."
+    println "Pipeline job '${jobName}' already exists inside folder '${folderName}'. Updating script..."
 }
 
-// Define GitHub repository as the SCM source
-def scm = new GitSCM(
-    [new UserRemoteConfig(gitUrl, null, null, null)], // Repo URL
-    [new BranchSpec(branch)], // Branch
-    false,
-    [],
-    null,
-    null,
-    [] // Additional behaviours
-)
+// Define the GitHub-to-Bitbucket synchronization pipeline script
+def pipelineScript = """
+pipeline {
+    agent any
 
-// Set the pipeline to use Jenkinsfile from GitHub
-def scmFlowDefinition = new CpsScmFlowDefinition(scm, "Jenkinsfile") // Looks for 'Jenkinsfile' in the repo
-job.definition = scmFlowDefinition
+    environment {
+        GITHUB_CREDENTIALS = 'github-token'
+        BITBUCKET_CREDENTIALS = 'bitbucket-jenkins'  // Make sure this matches Jenkins credentials ID
+    }
 
-// Save the job
+    stages {
+        stage('Setup Environment') {
+            steps {
+                sh 'git config --global --add safe.directory $(pwd)'
+            }
+        }
+
+        stage('Checkout GitHub') {
+            steps {
+                withCredentials([string(credentialsId: 'github-token', variable: 'GITHUB_TOKEN')]) {
+                    sh '''
+                        if [ -d ".git" ]; then
+                            echo "Repository already exists. Pulling latest changes..."
+                            git reset --hard
+                            git clean -fd
+                            git pull origin master
+                        else
+                            echo "Cloning repository..."
+                            git clone https://$GITHUB_TOKEN@github.com/abeciaj/hello-world.git .
+                            git checkout master
+                        fi
+                    '''
+                }
+            }
+        }
+
+        stage('Sync to Bitbucket') {
+            steps {
+                withCredentials([usernamePassword(credentialsId: 'bitbucket-jenkins', usernameVariable: 'BITBUCKET_USER', passwordVariable: 'BITBUCKET_PASS')]) {
+                    sh '''
+                        echo "Adding Bitbucket remote..."
+                        if git remote | grep -q bitbucket; then
+                            echo "Bitbucket remote exists. Updating..."
+                            git remote remove bitbucket
+                        fi
+                        
+                        git remote add bitbucket https://$BITBUCKET_USER:$BITBUCKET_PASS@bitbucket.org/devopsph/hello-world.git
+                        git config --global user.name "Jenkins"
+                        git config --global user.email "jenkins@example.com"
+                        
+                        echo "Pulling changes from Bitbucket..."
+                        git fetch bitbucket
+                        git pull bitbucket master --rebase
+                        
+                        echo "Pushing to Bitbucket..."
+                        git push -u bitbucket master
+                    '''
+                }
+            }
+        }
+    }
+
+    post {
+        success {
+            echo '✅ Pipeline completed successfully!'
+        }
+        failure {
+            echo '❌ Pipeline failed. Check logs for errors.'
+        }
+    }
+}
+"""
+
+// Assign the pipeline script to the job
+def flowDefinition = new CpsFlowDefinition(pipelineScript, true)
+job.setDefinition(flowDefinition)
 job.save()
-println "Pipeline job '${jobName}' in folder '${folderName}' is now connected to GitHub repo '${gitUrl}'."
+
+println "Pipeline script added/updated for job '${jobName}' inside folder '${folderName}'."
